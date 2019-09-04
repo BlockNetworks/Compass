ced55ec5f1448dead9fcb40a7403b048b52be7bc
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2019-09-03 22:28:39 -0700

    Create ShowBarTask.php

---
+++ src/Compass/ShowBarTask.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+namespace Compass;
+use pocketmine\Player;
+use pocketmine\scheduler\Task;
+class ShowBarTask extends Task{
+    /** @var CompassBar */
+    private $plugin;
+    public function __construct(CompassBar $plugin){
+        $this->plugin = $plugin;
+    }
+    public function onRun(int $currentTick){
+        $bossBar = $this->plugin->getBossBar();
+        /** @var Player $viewer */
+        foreach($bossBar->getViewers() as $viewer){
+            if(!$viewer->isClosed()){
+                $bossBar->setTitle(Utils::getCompass($viewer->yaw));
+                $bossBar->updateFor($viewer);
+            }else{
+                $viewer->removeBossbar(CompassBar::BOSSBAR_ID);
+            }
+        }
+    }
+    public function onCancel(){
+        $bossBar = $this->plugin->getBossBar();
+        /** @var Player $viewer */
+        foreach($bossBar->getViewers() as $viewer){
+            $viewer->removeBossbar(CompassBar::BOSSBAR_ID);
+        }
+    }
+}
